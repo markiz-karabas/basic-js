774a153640a6e235481e78e7369f7fcf3126b122
markiz-karabas <vsevolodova@inbox.ru>
2023-12-25 15:06:57 +0300

    feat: dream-team

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  let arr = [];
+
+  domains.forEach((item) => {
+    const arr = item.split('.').reverse();
+    let current = '';
+    arr.forEach((item) => {
+      current = current ? `${current}.${item}` : `.${item}`;
+      if (result[current]) {
+        result[current]++;
+      } else {
+        result[current] = 1;
+      }
+    }) 
+  })
+  return result;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false;
+  let baseLetters = [];
+  members.forEach((name) => {
+    if (typeof name === 'string') {
+      name = name.trim();
+      baseLetters.push(name[0].toUpperCase());
+    }
+  })
+  baseLetters.sort((prev, next) => prev.localeCompare(next));
+  return baseLetters.join('');
 }
 
 module.exports = {
